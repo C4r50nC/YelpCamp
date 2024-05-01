c341864e5fff87991c28bfc7378bfc8e553149b7
C4r50nC <xichen059@gmail.com>
2024-05-01 22:17:28 +1000

    feat: improve review rating with stars

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,8 +2,8 @@ const express = require("express");
 // Add mergeParams to ensure :id in the route prefix can be accessed as req.params.id
 const router = express.Router({ mergeParams: true });
 const { isLoggedIn, isReviewAuthor, validateReview } = require("../middleware");
-const reviews = require("../controllers/reviews");
 const catchAsync = require("../utils/catch-async");
+const reviews = require("../controllers/reviews");
 
 router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
