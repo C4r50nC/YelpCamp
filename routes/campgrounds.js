20cd9705283f4f745d9bdf087b6f420636a59f4a
C4r50nC <xichen059@gmail.com>
2024-05-01 21:39:20 +1000

    refactor: group campground routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,43 +1,37 @@
 const express = require("express");
 const router = express.Router();
-const campgrounds = require("../controllers/campgrounds");
 const { isAuthor, isLoggedIn, validateCampground } = require("../middleware");
-
 const catchAsync = require("../utils/catch-async");
+const campgrounds = require("../controllers/campgrounds");
 
-router.get("/", catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
+// Place before :id route to avoid "new" being read as ID
 
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
-
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
-);
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
