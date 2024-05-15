c3c3edc857d885a9443ae3392c66ab7089a47aa1
C4r50nC <xichen059@gmail.com>
2024-05-15 21:06:22 +1000

    refactor: move in-line map styles into stylesheet

--- public/js/show-page-map.js
+++ public/js/show-page-map.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = MAPBOX_TOKEN;
 const map = new mapboxgl.Map({
-  container: "map", // container ID
+  container: "details-map", // container ID
   style: "mapbox://styles/mapbox/light-v11", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
