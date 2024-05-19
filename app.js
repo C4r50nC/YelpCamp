7f0b69ef5a627b64ae61f9e70f3969dd2982a195
C4r50nC <xichen059@gmail.com>
2024-05-19 11:20:09 +1000

    feat: configure helmet

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
-// app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
